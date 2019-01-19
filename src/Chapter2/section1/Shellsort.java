81a1cec1e7df364c34d90e5ae502ed80cf5cbb1c
zkyyo <zkyyo@outlook.com>
2019-01-19 12:47:06 +0800

    实现希尔排序算法

---
+++ src/Chapter2/section1/Shellsort.java
@@ -0,0 +1,61 @@
+package Chapter2.section1;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import util.PathUtils;
+
+/**
+ * @author zkyyo
+ * @since 2019-01-19 12:33
+ **/
+public class Shellsort {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h < N / 3) {
+            h = 3 * h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                // j >= h 保证 j - h >= h - h = 0
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    exch(a, j, j - h);
+                }
+            }
+            h = h / 3;
+        }
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = new In(PathUtils.getFilePath("words3.txt")).readAllStrings();
+        sort(a);
+        System.out.println(isSorted(a));
+        show(a);
+    }
+}
